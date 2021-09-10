243b71aaab8b321ac79a95f297d77d13da2d7d5e
uQurmangazy <kurmangazy1994@gmail.com>
2021-09-10 15:52:48 +0600

    Добавить реализацию репозитория книг в памяти

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Store
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
+        Book[] GetAllByTitle(string titlePart);
     }
 }
