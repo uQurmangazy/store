ee9962e711f1e39ccbfad94dbd72fcd94ecb0df3
uQurmangazy <kurmangazy1994@gmail.com>
2021-09-13 11:21:32 +0600

    Создать веб-приложение

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Linq;
 
+// Data Access Layer
+// infrastucture потому что вместо sql там может быть что-то другое
 namespace Store.Memory
 {
+    // Реализация IBookRepository без sql вместо него массив памяти
     public class BookRepository : IBookRepository
     {
         private readonly Book[] books = new[]
@@ -11,9 +14,13 @@ namespace Store.Memory
             new Book(2,"Refactoring"),
             new Book(3, "C Programming Language")
         };
-        public Book[] GetAllByTitle(string titlePart)
+        public Book[] GetAllByTitle(string titlePart)// метод возвращает список, а точнее массив книг
         {
             return books.Where(book => book.Title.Contains(titlePart)).ToArray();//фильтрация массива по критерию
+            // критерий: возвращать массив книг, содержащих titlePart в названии
+            // для этого: берем массив books и выполняем команду Where, который позволяет отобрать по лямбде-выражения предикат
+            // Это функция с одним параметром book, который возвращает true когда у book есть Title, который содержит titlePart
+            //  titlePart ToArray
         }
     }
 }
